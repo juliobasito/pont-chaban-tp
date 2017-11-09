5c3b0a12634f952609014345559c5f08827ed5cd
juliobasito <bassecathalinat.jules@gmail.com>
2017-11-09 21:53:33 +0100

    Add search input in dev.

---
+++ src/components/search.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import SearchInput from './searchInput';
+
+class Search extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      inputValueBegin: 0,
+      inputValueEnd: 0
+    };
+  }
+
+  putNewDate = (newValue, nameInput) => {
+    if (nameInput === 'begin-date') {
+      this.setState({
+        inputValueBegin: newValue
+      })
+    }
+    if (nameInput === 'end-date') {
+      this.setState({
+        inputValueEnd: newValue
+      })
+    }
+  };
+
+  changeDate = () => {
+    if (this.state.inputValueBegin !== null || this.state.inputValueEnd !== null) {
+      this.props.changeApiDate(this.state.inputValueBegin, this.state.inputValueEnd)
+    }
+  };
+
+  render() {
+    return (
+      <div>
+      <SearchInput inputValue={this.state.inputValueBegin} putNewDate={this.putNewDate} nameInput="begin-date"/>
+      <SearchInput inputValue={this.state.inputValueEnd} putNewDate={this.putNewDate} nameInput="end-date"/>
+      <input type="submit" onClick={this.changeDate} />
+      </div>
+    );
+  }
+}
+
+export default Search;
---
+++ src/components/searchInput.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class searchInput extends Component {
+
+  handleChange = (event) => {
+    const newValue = event.target.value;
+    this.props.putNewDate(newValue, this.props.nameInput)
+  }
+
+  render() {
+    return (
+      <input type="date" value={this.props.inputValue} onChange={this.handleChange} name={this.props.nameInput}/>
+    );
+  }
+}
+
+export default searchInput;
